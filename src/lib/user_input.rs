61aab44b0a8b3d2414423d592f7c0bdfcc19fdc5
Rohit Dhill <s4dr0t1@protonmail.com>
2022-07-21 18:07:21 +0530

    Add support for program arguments

--- src/lib/user_input.rs
+++ src/lib/user_input.rs
@@ -24,18 +24,31 @@ mod private {
     ///Run the command based on the user input
     pub fn run_command()
     {
+        //Take the user input with the help of take_user_input() function
         let user_input = take_user_input();
+
+        //match user_input to see if we actually get some input, or an Error
         match user_input {
-            
-            //If we get a string from user input, call the command
+            //If we get a string from user input, call the command, but first split the whitespaces to separate the program name and the arguments
             Ok(input) => {
                 //Trim the user input so that it has no white-spaces
-                let command = input.trim();
-                let mut child = process::Command::new(command)
-                    .spawn()
-                    .unwrap(); //you need to handle this later, just keep it as as it now
-                //Don't accept any other command until the previous one is complete (blocking concept)
-                child.wait();
+                let mut command_identifiers = input.trim().split_whitespace();
+                let program = command_identifiers.next();
+
+                match program {
+                    Some(program) => {
+                        //println!("Do something"); //Change this with actual useful information
+                        let program_arguments = command_identifiers;
+                        let mut child = process::Command::new(program)
+                            .args(program_arguments)
+                            .spawn()
+                            .unwrap(); //you need to handle this later, just keep it as as it now
+                        //Don't accept any other command until the previous one is complete (blocking concept)
+                        child.wait();
+                    }
+                    None => println!("No input provided"),
+
+                }
             },
             //Otherwise handle the error
             Err(error) => println!("An error occurred: {}", error)
