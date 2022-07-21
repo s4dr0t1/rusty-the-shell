f2b5e722f50e62415a964be826b27195549ce388
Rohit Dhill <s4dr0t1@protonmail.com>
2022-07-21 21:51:29 +0530

    Code refactoring and better error handling

--- src/lib/user_input.rs
+++ src/lib/user_input.rs
@@ -1,19 +1,35 @@
+/*
+ * The main driving code of this module
+ * Does the following:
+     * Runs a command given in by the user
+     * Shows the user prompt
+     * Error handling
+     * Flushing the STDOUT buffer
+ */
 pub fn run_command()
 {
     loop {
         //The user prompt
-        use std::io::Write;
         println!("> ");
 
         //Flush STDOUT buffer
+        use std::io::Write;
         std::io::stdout().flush();
-        private::run_command();
+
+        //Run the command
+        match private::run_command()
+        {
+            Ok(_) => continue,
+            Err(some_error) => println!("An error occurred: {}", some_error),
+        }
     }
 }
 
 
 
-mod private {
+//This shit is private to other parts, defined in a private module for better readability
+mod private 
+{
     //External libraries and modules
     use std::
     {
@@ -21,51 +37,48 @@ mod private {
         process
     };
 
+
     ///Run the command based on the user input
-    pub fn run_command()
+    pub fn run_command() -> Result<(), anyhow::Error>
     {
-        //Take the user input with the help of take_user_input() function
-        let user_input = take_user_input();
+        //Take the user input
+        let user_input = take_user_input()?;
 
-        //match user_input to see if we actually get some input, or an Error
-        match user_input {
-            //If we get a string from user input, call the command, but first split the whitespaces to separate the program name and the arguments
-            Ok(input) => {
-                //Trim the user input so that it has no white-spaces
-                let mut command_identifiers = input.trim().split_whitespace();
-                let program = command_identifiers.next();
+        //Trim the user input so that it has no white-spaces
+        let mut command_identifiers = user_input.trim().split_whitespace();
+        //Define program = name of the program we're trying to execute
+        let program = command_identifiers.next();
 
-                match program {
-                    Some(program) => {
-                        //println!("Do something"); //Change this with actual useful information
-                        let program_arguments = command_identifiers;
-                        let mut child = process::Command::new(program)
-                            .args(program_arguments)
-                            .spawn()
-                            .unwrap(); //you need to handle this later, just keep it as as it now
-                        //Don't accept any other command until the previous one is complete (blocking concept)
-                        child.wait();
-                    }
-                    None => println!("No input provided"),
-
-                }
+        match program {
+            Some(program) => {
+                let program_arguments = command_identifiers;
+                let mut child = process::Command::new(program)
+                    .args(program_arguments)
+                    .spawn()?;
+                    //.unwrap(); //you need to handle this later, just keep it as as it now
+                //Don't accept any other command until the previous one is complete (blocking concept)
+                child.wait()?;
+                return Ok(());
             },
-            //Otherwise handle the error
-            Err(error) => println!("An error occurred: {}", error)
+            None => 
+            {
+                println!("No input provided");
+                return Ok(());
+            }
+
         }
     }
-
 
     ///Take in user input from the user and return an error otherwise
     fn take_user_input() -> Result<String, io::Error>
     {
-        //Define the variable which will be used to hold user input
+        //Variable used to store user input
         let mut user_input = String::new();
 
         //Take input from the user
         io::stdin().read_line(&mut user_input)?;
 
+        //Return the user input (in case of any error, the ? operator will deal with it)
         Ok(user_input)
     }
 }
-
